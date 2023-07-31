c87eca1b57315abf15677300d77bc02d1be8a698
xla344 <xla344@sfu.ca>
2023-07-30 23:15:38 -0700

    clean up unused code in back-end and keep the entity folder for future use of databased if needed

--- back-end/src/app.py
+++ back-end/src/app.py
@@ -1,5 +1,4 @@
 from .entities.entity import Session, engine, Base
-from .entities.exam import Exam, ExamSchema
 from .docdict.docdict import init_dict, searchText
 from flask import Flask, jsonify, request
 from flask_cors import CORS
@@ -9,23 +8,13 @@ app = Flask(__name__)
 CORS(app)
 
 # generate database schema
-Base.metadata.create_all(engine)
+# Base.metadata.create_all(engine)
 
 @app.before_request
 def before_request_handler():
     init_dict()
 
-# @app.route('/init', methods=['GET'])
-# def get_dictionary():
-#     init_dict()
-#     return jsonify({}), 200
-
-@app.route('/test')
-def get_test():
-    data = {"message": "hello world"}
-    return jsonify(data)
-
-@app.route('/initial_text/<string:filename>')
+@app.route('/initial_text/<string:filename>', methods=['GET'])
 def init_text(filename):
     try:
         with open(f'./doc/{filename}.txt', 'r') as file:
@@ -34,63 +23,10 @@ def init_text(filename):
     except FileNotFoundError:
         return jsonify({"error": "File not found"}), 404
 
-@app.route('/search_text/<string:filename>/<string:searchContent>')
+@app.route('/search_text/<string:filename>/<string:searchContent>', methods=['GET'])
 def search_text(filename, searchContent):
     try:
         response = searchText(filename, searchContent)
         return jsonify(response), 200
     except FileNotFoundError:
         return jsonify({"error": "File not found"}), 404
-
-@app.route('/exams')
-def get_exams():
-    # fetching from the database
-    session = Session()
-    exam_objects = session.query(Exam).all()
-
-    # transforming into JSON-serializable objects
-    schema = ExamSchema(many=True)
-    exams = schema.dump(exam_objects)
-
-    # serializing as JSON
-    session.close()
-    return jsonify(exams.data)
-
-@app.route('/exams', methods=['POST'])
-def add_exam():
-    # mount exam object
-    posted_exam = ExamSchema(only=('title', 'description'))\
-        .load(request.get_json())
-
-    exam = Exam(**posted_exam.data, created_by="HTTP post request")
-
-    # persist exam
-    session = Session()
-    session.add(exam)
-    session.commit()
-
-    # return created exam
-    new_exam = ExamSchema().dump(exam).data
-    session.close()
-    return jsonify(new_exam), 201
-
-# # start session
-# session = Session()
-
-# # check for existing data
-# exams = session.query(Exam).all()
-
-# if len(exams) == 0:
-#     # create and persist mock exam
-#     python_exam = Exam("mysql check", "Check if mysql works correctly.", "script")
-#     session.add(python_exam)
-#     session.commit()
-#     session.close()
-
-#     # reload exams
-#     exams = session.query(Exam).all()
-
-# # show existing exams
-# print('### Exams:')
-# for exam in exams:
-#     print(f'({exam.id}) {exam.title} - {exam.description}')
